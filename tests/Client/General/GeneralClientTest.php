2b7560648ffd23747b06c610b81a86ec8585cb26
Ryabov <andrei.ryabov@x2sy.com>
2021-01-22 00:20:32 +0200

    add phpunit

---
+++ tests/Client/General/GeneralClientTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Payourself2\Bundle\MonobankBundle\Tests\Client\General;
+
+use PHPUnit\Framework\TestCase;
+
+class GeneralClientTest extends TestCase
+{
+    public function test():void
+    {
+       self::assertTrue(true);
+    }
+}
