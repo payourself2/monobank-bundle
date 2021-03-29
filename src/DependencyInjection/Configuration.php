ae63cb7e3c5297b50f912ebb792ba4dfc858d5ef
Ryabov <andrei.ryabov@x2sy.com>
2021-03-29 09:11:59 +0300

    update configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,31 +4,33 @@ declare(strict_types=1);
 
 namespace Payourself2\Bundle\MonobankBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('payourself2_monobank');
+        /** @var ArrayNodeDefinition $parametersNode */
+        $parametersNode = $treeBuilder->getRootNode();
 
-        $treeBuilder->getRootNode()
-            ->addDefaultsIfNotSet()
-            ->children()
-            ->scalarNode('api_base_path')
+        $ch = $parametersNode->addDefaultsIfNotSet()->children();
+        $ch->scalarNode('api_base_path')
             ->defaultValue('https://api.monobank.ua')
-            ->end()
-            ->scalarNode('personal_key')
-            ->defaultValue('')
-            ->end()
-            ->scalarNode('pub_key')
-            ->defaultValue('')
-            ->end()
-            ->scalarNode('priv_key')
-            ->defaultValue('')
-            ->end()
             ->end();
+        $ch->scalarNode('personal_key')
+            ->defaultValue('')
+            ->end();
+        $ch->scalarNode('pub_key')
+            ->defaultValue('')
+            ->end();
+        $ch->scalarNode('priv_key')
+            ->defaultValue('')
+            ->end();
+
+        $ch->end();
 
         return $treeBuilder;
     }
