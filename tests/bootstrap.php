32ebfc754388d75c256292a10c6960fad50ce42b
Ryabov <andrei.ryabov@x2sy.com>
2021-04-01 08:02:17 +0300

    add const in correct way

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,3 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
     require dirname(__DIR__) . '/config/bootstrap.php';
 }
-
-if(!defined('PAYOURSELF2_FIXTURES_PATH')){
-    define('PAYOURSELF2_FIXTURES_PATH', __DIR__ . '/Fixtures');
-}
